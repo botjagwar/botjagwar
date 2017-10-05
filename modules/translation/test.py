cd297c7c93b81625d1fd44b1aa27e11bc48d554d
Rado Andrianjanahary <rado.md92@gmail.com>
2017-10-05 22:50:40 +0200

    test skeleton for translation

--- modules/translation/test.py
+++ modules/translation/test.py
@@ -1,14 +1,14 @@
-
-from core import Translation
+# coding: utf8
 
 data_file = "conf/dikantenyvaovao/"
 
 
 class TestTranslation(object):
     def test_translate_english(self):
-        listPages = [""]
-        test = Translation()
-        listpagestring = parseErrlog()
-        for page in listpagestring:
-            page = page.decode('utf8')
-            test.process_wiktionary_page('en', Page)
+        pass
+
+    def test_translate_french(self):
+        pass
+
+    def test_translate_unknown(self):
+        pass
