3e2ecc511e774d22c54d6affc70866afe6a145c7
Rado Andrianjanahary <rado.md92@gmail.com>
2017-10-05 22:44:38 +0200

    tests: code skeletons

--- modules/translation/test.py
+++ modules/translation/test.py
@@ -1,34 +1,14 @@
-import re
 
-import pywikibot as pwbot
 from core import Translation
 
 data_file = "conf/dikantenyvaovao/"
 
-def testTranslate(**args):
-    test = Translation()
-    verbose = True
-    listpagestring = parseErrlog()
-    for page in listpagestring:
-        page = page.decode('utf8')
-        for lang in ['en', 'fr']:
-            print lang
-            print "------------------"
-            Page = pwbot.Page(pwbot.Site(lang, 'wiktionary'), page)
-            test.process_wiktionary_page(lang, Page)
 
-
-def parseErrlog():
-    ret = []
-    in_file = file(data_file + 'dikantenyvaovao.exceptions', 'r').readlines()
-    for item in in_file:
-        pwbot.output(item)
-        item = item.strip('\n')
-
-        regex = re.search("\[\[(.*)\]\]", item)
-        if regex is None:
-            continue
-
-        string = regex.groups()[0]
-        ret.append(string[3:-3])
-    return ret
+class TestTranslation(object):
+    def test_translate_english(self):
+        listPages = [""]
+        test = Translation()
+        listpagestring = parseErrlog()
+        for page in listpagestring:
+            page = page.decode('utf8')
+            test.process_wiktionary_page('en', Page)
---
+++ test.py
@@ -0,0 +1,8 @@
+from modules.entryprocessor.test import (
+    TestFRWiktionary,
+    TestDEWiktionary,
+    TestENWiktionary)
+
+from modules.translation.test import (
+
+)
