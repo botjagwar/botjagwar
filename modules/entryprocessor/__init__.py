785affb480424a14599ddafad1997c4cfdf318cb
Rado Andrianjanahary <rado.md92@gmail.com>
2017-10-05 22:43:22 +0200

    entryprocessor: factory, move dump processor

---
+++ modules/entryprocessor/__init__.py
@@ -0,0 +1,41 @@
+import inspect
+import sys
+import warnings
+
+from wiki.base import WiktionaryProcessor
+from wiki.de import DEWiktionaryProcessor
+from wiki.en import ENWiktionaryProcessor
+from wiki.fr import FRWiktionaryProcessor
+from wiki.mg import MGWiktionaryProcessor
+from wiki.nl import NLWiktionaryProcessor
+from wiki.pl import PLWiktionaryProcessor
+from wiki.ru import RUWiktionaryProcessor
+from wiki.sv import SVWiktionaryProcessor
+from wiki.vo import VOWiktionaryProcessor
+from wiki.zh import ZHWiktionaryProcessor
+
+
+data_file = 'conf/entryprocessor/'
+verbose = True
+
+
+class WiktionaryProcessorFactory(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def create(language):
+        assert type(language) in [str, unicode]
+        ct_module = sys.modules[__name__]
+        classes = inspect.getmembers(ct_module, inspect.isclass)
+        processors = [x for x in classes if x[0].endswith('WiktionaryProcessor')]
+        language_class_name = "%sWiktionaryProcessor" % language.upper()
+
+        for current_class_name, processor_class in processors:
+            if current_class_name == language_class_name:
+                return processor_class
+
+        warnings.warn("Tsy nahitana praosesera: '%s'" % language_class_name, Warning)
+        return WiktionaryProcessor
+
+__all__ = ["WiktionaryProcessorFactory"]
---
+++ modules/entryprocessor/dump.py
@@ -0,0 +1,44 @@
+# coding: utf8
+
+import pywikibot
+from __init__ import WiktionaryProcessorFactory
+from wiki.base import data_file
+
+
+class DumpPagegenerator(object):
+    def __init__(self, dumpfile, language):
+        self.dumpfile = None
+        self.list_translations = []
+        self.newentries = []
+        self.language = language
+        self.alltranslations = {}
+        self.file = pywikibot.xmlreader.XmlDump(dumpfile)
+        self.langblacklist = ['fr', 'en', 'sh', 'de', 'zh']
+
+    def load(self, dumptype, filename, language):
+        if dumptype == 'dump':
+            self.load_dump(data_file + filename, language)
+        elif dumptype == 'titles':
+            self.load_titlefile(data_file + filename, language)
+            self.process = self.process_titlelist
+
+    def load_dump(self, filename, language='en'):
+        self.language = language
+        self.file = pywikibot.xmlreader.XmlDump(filename)
+
+    def load_titlefile(self, filename, language='en'):
+        self.language = language
+        self.file = file(filename, 'r').readlines()
+
+    def get_processed_pages(self):
+        wiktionary_processor = WiktionaryProcessorFactory().create(self.language)
+
+        print("getting entry translation")
+        for fileentry in self.file.parse():
+            wiktionary_processor.process(fileentry.text, fileentry.title)
+            yield wiktionary_processor.retrieve_translations()
+
+        print("getting entry page")
+        for fileentry in self.file.parse():
+            wiktionary_processor.process(fileentry.text, fileentry.title)
+            yield wiktionary_processor.getall()
