22dd5e6f23939df81ce04681f3e1fe7e109b4c6a
Rado Andrianjanahary <rado.md92@gmail.com>
2017-10-05 23:17:07 +0200

    more test skeletons

--- test.py
+++ test.py
@@ -2,3 +2,35 @@ from modules.entryprocessor.test import (
     TestFRWiktionary,
     TestDEWiktionary,
     TestENWiktionary)
+
+class TestDikantenyVaovaoBot(object):
+    def test_get_origin_wiki(self):
+        pass
+
+    def test_update_statistics(self):
+        pass
+
+    def test_process_wiktionary_page(self):
+        # mock page.get
+        # mock page.put
+        pass
+
+class TestDikantenyVaovaoProcessWiktionaryPage(object):
+    pass
+
+class TestTranslation(object):
+    def test_code_to_name(self):
+        pass
+
+    def test_generate_redirections(self):
+        pass
+
+    def test_translation_from_bridge_language(self):
+        pass
+
+    def test_process_wiktionary_page_english(self):
+        pass
+
+    def test_process_wiktionary_page_french(self):
+        pass
+
