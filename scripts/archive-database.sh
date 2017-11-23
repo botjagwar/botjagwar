34756955b5d58b4711fab7a84b470571ea6db82f
Rado Andrianjanahary <rado.md92@gmail.com>
2017-11-23 22:36:27 +0100

    fix scripts

--- scripts/archive-database.sh
+++ scripts/archive-database.sh
@@ -4,9 +4,15 @@ cd $HOME/botjagwar
 
 DBPASS=$(cat conf/BJDBModule/database_password | base64 -d)
 ARCHIVEREPOSITORY=$(cat conf/BJDBModule/archive_repo)
+SQLREP=$ARCHIVEREPOSITORY/sql
+UNKNOWNWORDSREP=$ARCHIVEREPOSITORY/unknowns
 
-if [ ! -d "$ARCHIVEREPOSITORY" ]; then
-  mkdir -p $ARCHIVEREPOSITORY
+if [ ! -d "$SQLREP" ]; then
+  mkdir -p $SQLREP
+  if [ ! -d "$UNKNOWNWORDSREP" ]; then
+    mkdir -p $UNKNOWNWORDSREP
+  fi
 fi
 
-mysqldump --all-databases --password=$DBPASS --user=root | gzip -f > $ARCHIVEREPOSITORY/$(date -u +%Y-%m-%d).gz
+mysqldump --all-databases --password=$DBPASS --user=root | gzip -f > $SQLREP/$(date -u +%Y-%m-%d).sql.gz
+cat user_data/dikantenyvaovao/word_hits | gzip -f > $UNKNOWNWORDSREP/$(date -u +%Y-%m-%d).txt.gz
