32408be1c1208da4140db6855eede894fc7a3d6b
Rado Andrianjanahary <rado.md92@gmail.com>
2017-12-17 02:29:05 +0100

    getattr not found

--- models/__init__.py
+++ models/__init__.py
@@ -7,6 +7,12 @@ class BaseEntry(object):
         for attribute, value in properties.items():
             self.add_attribute(attribute, value)
 
+    def __getattr__(self, item):
+        if item in self.properties:
+            return self.properties[item]
+        else:
+            raise AttributeError('Attribute not found')
+
     def add_attribute(self, name, value):
         if name not in self.types and not self._additional:
             raise AttributeError("Unspecified Attribute '%s' not allowed in '%s' object" % (
