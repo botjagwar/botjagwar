23f6573804e01bdf02f5053f51607a582890925b
Rado Andrianjanahary <rado.md92@gmail.com>
2018-02-01 00:34:54 +0100

    issue #10: move all relations classes in separate file

---
+++ database/__init__.py
@@ -0,0 +1,51 @@
+
+from sqlalchemy import Table, Column, ForeignKey
+from sqlalchemy import Integer, String
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
+dictionary_association = Table('dictionary', Base.metadata,
+    Column('word', Integer, ForeignKey('word.id')),
+    Column('definition', Integer, ForeignKey('definitions.id'))
+)
+
+
+class Word(Base):
+    __tablename__ = 'word'
+    id = Column(Integer, primary_key=True)
+    word = Column(String(150))
+    language = Column(String(6))
+    part_of_speech = Column(String(15))
+    definitions = relationship(
+        "Definition",
+        secondary=dictionary_association,
+        back_populates="words")
+
+    def serialise(self):
+        word_data = {
+            'word': self.word,
+            'definitions': [definition.serialise() for definition in self.definitions],
+        }
+        print (self.definitions)
+        return word_data
+
+
+class Definition(Base):
+    __tablename__ = 'definitions'
+    id = Column(Integer, primary_key=True)
+    definition = Column(String(250))
+    definition_language = Column(String(6))
+    words = relationship(
+        'Word',
+        secondary=dictionary_association,
+        back_populates='definitions')
+
+    def serialise(self):
+        definition_data = {
+            'definition': self.definition,
+            'language': self.definition_language
+        }
+        return definition_data
--- dictionary.py
+++ dictionary.py
@@ -3,65 +3,16 @@ from aiohttp.web import Response
 
 import json
 
-from sqlalchemy import Table, Column, ForeignKey
-from sqlalchemy import Integer, String
-
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import sessionmaker
+from database import Base, Definition, Word
+
 
 DATABASE_STORAGE_INFO_FILE = 'data/storage_info'
 with open(DATABASE_STORAGE_INFO_FILE) as storage_file:
     STORAGE = storage_file.read()
 ENGINE = create_engine('sqlite:///%s' % STORAGE)
-Base = declarative_base()
 routes = web.RouteTableDef()
-
-
-dictionary_association = Table('dictionary', Base.metadata,
-    Column('word', Integer, ForeignKey('word.id')),
-    Column('definition', Integer, ForeignKey('definitions.id'))
-)
-
-
-class Word(Base):
-    __tablename__ = 'word'
-    id = Column(Integer, primary_key=True)
-    word = Column(String(150))
-    language = Column(String(6))
-    part_of_speech = Column(String(15))
-    definitions = relationship(
-        "Definition",
-        secondary=dictionary_association,
-        back_populates="words")
-
-    def serialise(self):
-        word_data = {
-            'word': self.word,
-            'definitions': [definition.serialise() for definition in self.definitions],
-        }
-        print (self.definitions)
-        return word_data
-
-
-class Definition(Base):
-    __tablename__ = 'definitions'
-    id = Column(Integer, primary_key=True)
-    definition = Column(String(250))
-    definition_language = Column(String(6))
-    words = relationship(
-        'Word',
-        secondary=dictionary_association,
-        back_populates='definitions')
-
-    def serialise(self):
-        definition_data = {
-            'definition': self.definition,
-            'language': self.definition_language
-        }
-        return definition_data
-
-
 Base.metadata.create_all(ENGINE)
 SessionClass = sessionmaker(bind=ENGINE)
 
@@ -101,6 +52,7 @@ async def add_entry(request):
     session.add(definition)
     session.add(word)
     session.commit()
+    session.flush()
     return Response(status=200, text='OK BOSS')
 
 
