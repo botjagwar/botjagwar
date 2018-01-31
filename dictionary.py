fb3be710bd53f3eab7a02d4e66243d8be0a83efc
Rado Andrianjanahary <rado.md92@gmail.com>
2018-02-01 00:25:58 +0100

    issue #10: implement add feature using sqlalchemy

--- dictionary.py
+++ dictionary.py
@@ -1,14 +1,14 @@
 from aiohttp import web
 from aiohttp.web import Response
 
-from sqlalchemy import Boolean
-from sqlalchemy import Column
-from sqlalchemy import Integer
-from sqlalchemy import String
+import json
+
+from sqlalchemy import Table, Column, ForeignKey
+from sqlalchemy import Integer, String
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 DATABASE_STORAGE_INFO_FILE = 'data/storage_info'
 with open(DATABASE_STORAGE_INFO_FILE) as storage_file:
@@ -18,55 +18,110 @@ Base = declarative_base()
 routes = web.RouteTableDef()
 
 
-class Dictionary(Base):
-    __tablename__ = 'dictionary'
+dictionary_association = Table('dictionary', Base.metadata,
+    Column('word', Integer, ForeignKey('word.id')),
+    Column('definition', Integer, ForeignKey('definitions.id'))
+)
+
+
+class Word(Base):
+    __tablename__ = 'word'
     id = Column(Integer, primary_key=True)
     word = Column(String(150))
     language = Column(String(6))
-    definition = Column(String(250))
-    definition_language = Column(String(6))
     part_of_speech = Column(String(15))
+    definitions = relationship(
+        "Definition",
+        secondary=dictionary_association,
+        back_populates="words")
 
-    def __init__(self):
-        pass
+    def serialise(self):
+        word_data = {
+            'word': self.word,
+            'definitions': [definition.serialise() for definition in self.definitions],
+        }
+        print (self.definitions)
+        return word_data
 
 
-class GeneratedDictionary(Base):
-    __tablename__ = 'generated_dictionary'
+class Definition(Base):
+    __tablename__ = 'definitions'
     id = Column(Integer, primary_key=True)
-    word = Column(String(150))
-    language = Column(String(6))
     definition = Column(String(250))
     definition_language = Column(String(6))
-    part_of_speech = Column(String(15))
-    verified = Column(Boolean, default=False)
+    words = relationship(
+        'Word',
+        secondary=dictionary_association,
+        back_populates='definitions')
+
+    def serialise(self):
+        definition_data = {
+            'definition': self.definition,
+            'language': self.definition_language
+        }
+        return definition_data
 
 
-class DatabaseServiceHandler(object):
-    def __init__(self):
-        Base.metadata.create_all(ENGINE)
-        self.db_session = sessionmaker(bind=ENGINE)
-
-    def __del__(self):
-        self.db_session.close_all()
+Base.metadata.create_all(ENGINE)
+SessionClass = sessionmaker(bind=ENGINE)
 
 
-database = DatabaseServiceHandler()
+@routes.get('/entry/{language}/{word}')
+async def get_entry(request):
+    """
+    Return a list of entries matching the word and the language.
+    :param request:
+    :return:
+        HTTP 200 if the word exists
+        HTTP 404 otherwise
+    """
+    session = SessionClass()
+    objects = session.query(Word).filter_by(
+        word=request.match_info['word'],
+        language=request.match_info['language']).all()
+    jsons = [objekt.serialise() for objekt in objects]
+    return Response(text=json.dumps(jsons), status=200)
 
 
-@routes.get('/definition/{language}/{word}')
-async def get_definition(request):
-    return Response(text='get definition')
+@routes.get('/entry/{language}/{word}/add')
+async def add_entry(request):
+    """
+    Adds an antry to the dictionary.
+    :param request:
+    :return:
+        HTTP 200 if entry is OK
+    """
+    # FIXME
+    session = SessionClass()
+    definition = Definition(definition='sdflksf', definition_language='el')
+    word = Word(word=request.match_info['word'],
+                language=request.match_info['language'],
+                part_of_speech='ana',
+                definitions=[definition])
+    session.add(definition)
+    session.add(word)
+    session.commit()
+    return Response(status=200, text='OK BOSS')
 
-@routes.post('/definition/{language}/{word}/edit')
+
+@routes.post('/entry/{language}/{word}/edit')
+async def edit_entry(request):
+    data = await request.json()
+    session = SessionClass()
+    # ... code ...
+    session.commit()
+
+
+
+@routes.post('/entry/{language}/{word}/append')
 async def edit_definition(request):
-    return Response(text='post definition')
+    data = await request.json()
+    session = SessionClass()
+    # ... code ...
+    session.commit()
 
-@routes.post('/definition/{language}/{word}/append')
-async def edit_definition(request):
-    return Response(text='append new definition')
 
-@routes.delete('/definition/{language}/{word}/delete')
+@routes.delete('/entry/{language}/{word}/delete')
 async def delete_definition(request):
     return Response(text='delete matching definition')
 
