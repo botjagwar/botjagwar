5afdc842b4f2e74f1fd0b873e08add7ff8634416
Rado Andrianjanahary <rado.md92@gmail.com>
2018-02-24 23:01:27 +0100

    backend is automatically spawned

--- scripts/entry-translator-start.sh
+++ scripts/entry-translator-start.sh
@@ -5,14 +5,6 @@ set -e
 cd $HOME/botjagwar
 INTERPRETER=python3.6
 
-if ! ps aux | grep python | grep entry_translator
-then
-	$INTERPRETER dictionary.py &
-	sleep 2
-	$INTERPRETER entry_translator.py &
-	sleep 1
-fi
-
 if ! ps aux | grep python | grep wiktionary_irc
 then
 	$INTERPRETER wiktionary_irc.py & echo $! > /tmp/ircbot.pid
