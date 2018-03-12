1788a05177ac1320685aac9f8434d69ba2c5fd71
Rado Andrianjanahary <rado.md92@gmail.com>
2018-03-12 23:54:02 +0100

    simplify thread
    Translation engine got blocked somehow, a simpler decorator for threaded function may prevent that from happening. Wait and see.

--- modules/decorator.py
+++ modules/decorator.py
@@ -4,24 +4,14 @@ import time
 
 
 def threaded(f):
-    import queue
-    def wrapped_f(q, *args, **kwargs):
-        '''this function calls the decorated function and puts the
-        result in a queue'''
-        ret = f(*args, **kwargs)
-        q.put(ret)
-
     def wrap(*args, **kwargs):
         '''this is the function returned from the decorator. It fires off
         wrapped_f in a new thread and returns the thread object with
         the result queue attached'''
 
-        q = queue.Queue()
-
-        t = threading.Thread(target=wrapped_f, args=(q,)+args, kwargs=kwargs)
+        t = threading.Thread(target=f, args=args, kwargs=kwargs)
         t.daemon = False
         t.start()
-        t.result_queue = q
         return t
 
     return wrap
