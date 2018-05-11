353a919c830cb4d22cfc6c2044a2e102f9391608
Rado Andrianjanahary <rado.md92@gmail.com>
2018-05-11 15:11:20 +0200

    rearrange entryprocessor tests

--- test/unit_tests/test_entryprocessor/__init__.py
+++ test/unit_tests/test_entryprocessor/__init__.py
@@ -1,4 +1,3 @@
-import unittest
 from test_utils.mocks import PageMock, SiteMock
 from api.entryprocessor import WiktionaryProcessorFactory
 
@@ -29,38 +28,6 @@ class GenericEntryProcessorTester:
             assert isinstance(entries, list), entries
             for t in entries:
                 assert len(t) == 4, t
-
-
-class TestEnglishWiktionaryEntryprocessor(GenericEntryProcessorTester, unittest.TestCase):
-    def setUp(self):
-        self.setup_for_language('en', ['eau', 'air', 'газета', 'geloof'])
-
-    def test_retrieve_translations_data_output(self):
-        page = PageMock(SiteMock(self.language, 'wiktionary'), 'air')
-        self.processor.process(page)
-        entries = self.processor.retrieve_translations()
-        entry = entries[-1]
-        word, pos, lang, definition = entry
-        self.assertEqual(word, '공기')
-        self.assertEqual(pos, 'mat')
-        self.assertEqual(lang, 'ko')
-        self.assertEqual(definition, "The substance constituting earth's atmosphere")
-
-
-class TestFrenchWiktionaryEntryprocessor(GenericEntryProcessorTester, unittest.TestCase):
-    def setUp(self):
-        self.setup_for_language('fr', ['eau', 'air', 'газета', 'geloof'])
-
-    def test_retrieve_translations_data_output(self):
-        page = PageMock(SiteMock(self.language, 'wiktionary'), 'air')
-        self.processor.process(page)
-        entries = self.processor.retrieve_translations()
-        entry = entries[-1]
-        word, pos, lang, definition = entry
-        self.assertEqual(word, '공기')
-        self.assertEqual(pos, 'ana')
-        self.assertEqual(lang, 'ko')
-        self.assertTrue(definition.startswith('mélange'))
 
 #
 # class TestMalagasyWiktionaryEntryprocessor(GenericEntryProcessorTester, unittest.TestCase):
