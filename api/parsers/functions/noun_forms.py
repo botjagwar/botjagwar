a093285e527f54ec0dd4b66827c1ab2eb2de5152
Rado Andrianjanahary <rado.md92@gmail.com>
2018-05-17 23:51:19 +0200

    [#14] finnish noun forms parser

--- api/parsers/functions/noun_forms.py
+++ api/parsers/functions/noun_forms.py
@@ -74,3 +74,25 @@ def parse_inflection_of(out_class):
         return out_class(lemma=lemma, case=case_name, number=number_, gender=gender)
 
     return _parse_inflection_of_partial
+
+
+def parse_fi_form_of(template_expression):
+    '{{fi-form of|näverrin|case=nominative|pl=plural}}'
+    for char in '{}':
+        template_expression = template_expression.replace(char, '')
+
+    parts = template_expression.split('|')
+    number = case = None
+    for tparam in parts:
+        if tparam.startswith('pl='):
+            number = tparam[3:]
+        if tparam.startswith('case='):
+            case = tparam[5:]
+
+    if '=' in parts[1]:
+        lemma = parts[-1]
+    else:
+        lemma = parts[1]
+
+    verb_form = NounForm(lemma, case, number, '')
+    return verb_form
