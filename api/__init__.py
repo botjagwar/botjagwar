949a7bff0ffd35f7d873d8c9ddd269fca7f063fa
Rado Andrianjanahary <rado.md92@gmail.com>
2018-05-18 23:22:14 +0200

    delete newline

--- api/__init__.py
+++ api/__init__.py
@@ -7,7 +7,7 @@ def get_version():
         return VERSION
     else:
         with open('data/version', 'r') as f:
-            VERSION = f.read()
+            VERSION = f.read().strip('\n')
     return VERSION
 
 
