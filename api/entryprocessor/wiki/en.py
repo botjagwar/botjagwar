f4989ae6d99fd733f07fa7fd31f21bf1bf59042c
Rado Andrianjanahary <rado.md92@gmail.com>
2018-05-29 01:46:30 +0200

    cosmit, try to add etymology

--- api/entryprocessor/wiki/en.py
+++ api/entryprocessor/wiki/en.py
@@ -81,11 +81,18 @@ class ENWiktionaryProcessor(WiktionaryProcessor):
 
     def getall(self, definitions_as_is=False):
         items = []
-        c = self.content
-        c = re.sub('Etymology', '', c)  # famafana ny etimilojia
-        c = re.sub("{{l/en\|(.*)}}", "\\1 ", c)  # fanovana ny endrika {{l/en}}
-        for l in re.findall("[\n]?==[ ]?([A-Za-z]+)[ ]?==\n", c):
-            c = c[c.find('==%s==' % l):]
+        content = self.content
+        content = re.sub("{{l/en\|(.*)}}", "\\1 ", content)  # remove {{l/en}}
+        for l in re.findall("[\n]?==[ ]?([A-Za-z]+)[ ]?==\n", content):
+            content = content[content.find('==%s==' % l):]
+            etymology_section_start = content.find('===Etymology===')
+            etymology_section = ''
+            if etymology_section_start != -1:
+                etymology_section_end = content.find('===', etymology_section_start)
+                if etymology_section_end != -1:
+                    etymology_section = content[etymology_section_start:etymology_section_end]
+
+            content = re.sub('Etymology', '', content)  # remove etymology section
 
             # pos
             pos = ''
@@ -95,17 +102,17 @@ class ENWiktionaryProcessor(WiktionaryProcessor):
             regex_ptext = regex_ptext.strip('|')
 
             ptext = '\n={4}[ ]?(%s)[ ]?={4}\n' % regex_ptext
-            if re.search(ptext, c) is None:
+            if re.search(ptext, content) is None:
                 ptext = '\n={3}[ ]?(%s)[ ]?={3}\n' % regex_ptext
-            posregex = re.findall(ptext, c)
+            posregex = re.findall(ptext, content)
 
             if not definitions_as_is:
                 for regex, replacement in self.regexesrep:
-                    c = re.sub(regex, replacement, c)
+                    c = re.sub(regex, replacement, content)
 
             # definition
             try:
-                defin = c.split('\n#')[1]
+                defin = content.split('\n#')[1]
 
                 if defin.find('\n') != -1:
                     defin = defin[:defin.find('\n')]
@@ -147,13 +154,13 @@ class ENWiktionaryProcessor(WiktionaryProcessor):
                 pos = 'ana'
                 defin = defin[1:]
             if len(defin.strip()) < 1: continue
-            # FIXME: return an object instead of a tuple
             try:
                 i = Entry(
                     entry=self.Page.title(),
                     part_of_speech=pos,
                     language=self.lang2code(l),
-                    entry_definition=[defin.strip()]
+                    entry_definition=[defin.strip()],
+                    etymology=etymology_section,
                 )
                 items.append(i)
             except KeyError:
