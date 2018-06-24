92b91ee73bf29b19711cce9c39e25b4dfb73606f
Rado Andrianjanahary <rado.md92@gmail.com>
2018-06-24 19:30:31 +0200

    add more content in generated pages

--- category_generator.py
+++ category_generator.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3.6
+
 import pywikibot
 
 CATEGORIES = {
@@ -6,18 +8,24 @@ CATEGORIES = {
     "Matoanteny": ["Endriky ny matoanteny"],
 }
 
-languages = pywikibot.Category(pywikibot.Site('mg', 'wiktionary'), 'fiteny')
+wiki = pywikibot.Site('mg', 'wiktionary')
+languages = pywikibot.Category(wiki, 'fiteny')
+
 for language in languages.subcategories():
     print('>>>>>', language, '<<<<<')
     language_name = language.title().split(':')[1]
     for root, subcat_titles in CATEGORIES.items():
         for subcat_element in subcat_titles:
             subcat_title = "%s amin'ny teny %s" % (subcat_element, language_name)
-            subcat = pywikibot.Category(pywikibot.Site('mg', 'wiktionary'), subcat_title)
+            subcat = pywikibot.Category(wiki, subcat_title)
+
             if not subcat.isEmptyCategory():
                 print('sokajy misy zavatra')
-                content = '[[sokajy:%s]]\n' % subcat_element
-                content += '[[sokajy:%s]]' % language_name
-                subcat.put(content, 'sokajy vaovao')
+                content = '{{abidy}}\n'
+                content += "* [[:mg:w:%s|%s]] eo amin'i [[:mg:|Wikipedia]]\n" % (subcat_element, subcat_element)
+                content += "* [[:mg:w:fiteny %s|Fiteny %s]] eo amin'i [[:mg:|Wikipedia]]\n" % (language_name, language_name)
+                content += '[[sokajy:%s|%s]]\n' % (subcat_element, language_name[0])
+                content += '[[sokajy:%s]]\n' % language_name
+                subcat.put(content, "Mamorona zanatsokajy ho an'ny karazanteny")
             else:
-                print('sokajy babangoana')
+                print(subcat_title, ': sokajy babangoana')
