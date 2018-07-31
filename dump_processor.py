6398d7e7009cacd56de2da895a1bfbd35c7603ff
Rado Andrianjanahary <rado.md92@gmail.com>
2018-07-31 23:41:45 +0200

    [Not tested, WIP] change dump processor to produce a db file

--- dump_processor.py
+++ dump_processor.py
@@ -4,8 +4,11 @@ from multiprocessing.dummy import Pool as ThreadPool
 
 from lxml import etree
 
+from api.data_caching import FastWordLookup, FastTranslationLookup
+from api.entryprocessor import WiktionaryProcessorFactory
 from api.output import Output
 from api.translation.core import Translation
+from object_model.word import Entry
 
 language = sys.argv[1]
 translation = Translation()
@@ -13,19 +16,42 @@ count = 0
 log = logging.getLogger(__name__)
 output = Output()
 
+processor_class = WiktionaryProcessorFactory.create(language)
+
+# Lookup table
+lookup_table = FastWordLookup()
+lookup_table.build_fast_word_tree()
+translation_lookup_table = FastTranslationLookup(language, 'mg')
+translation_lookup_table.build_table()
+
 
 def worker(xml_buffer):
     global count
     node = etree.XML(str(xml_buffer))
     title_node = node.xpath('//title')[0].text
-    print(count, ' >> ', title_node, ' <<')
     content_node = node.xpath('//revision/text')[0].text
 
     assert title_node is not None
     if ':' in title_node:
         return
 
-    translation.process_wiktionary_wikitext(title_node, language, content_node)
+    print(count, ' >> ', title_node, ' <<')
+    processor = processor_class()
+    processor.set_title(title_node)
+    processor.set_text(content_node)
+    entries = processor.getall()
+
+    for entry in entries:
+        if translation_lookup_table.lookup(entry):
+            translation = translation_lookup_table.translate(entry)
+            new_entry = Entry(
+                entry=entry.entry,
+                entry_definition=translation,
+                language=entry.language,
+                part_of_speech=entry.part_of_speech
+            )
+            print(new_entry)
+            output.db(new_entry)
 
 
 def process_dump():
