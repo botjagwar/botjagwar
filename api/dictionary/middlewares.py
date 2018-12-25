6580170b1a77298c3717888841f99740d5e88b3f
Rado Andrianjanahary <rado.md92@gmail.com>
2018-12-26 00:03:06 +0100

    autocommit: rollback on exception

--- api/dictionary/middlewares.py
+++ api/dictionary/middlewares.py
@@ -9,17 +9,21 @@ log = logging.getLogger(__name__)
 
 @middleware
 async def auto_committer(request, handler) -> Response:
-    response = await handler(request)
-    if request.method.lower() in ['post', 'put']:
-        if request.app['autocommit']:
-            if 400 <= response.status < 600:
-                request.app['session_instance'].rollback()
-                log.info('automatically rolled back changes to database')
-            else:
-                request.app['session_instance'].commit()
-                log.info('automatically committed changes to database')
-
-    return response
+    try:
+        response = await handler(request)
+        if request.method.lower() in ['post', 'put']:
+            if request.app['autocommit']:
+                if 400 <= response.status < 600:
+                    request.app['session_instance'].rollback()
+                    log.info('automatically rolled back changes to database')
+                else:
+                    request.app['session_instance'].commit()
+                    log.info('automatically committed changes to database')
+    except Exception as e:
+        request.app['session_instance'].rollback()
+        raise e
+    else:
+        return response
 
 
 @middleware
