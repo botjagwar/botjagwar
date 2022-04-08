b6f1ea2019f54ec3cb9fa9b125ef27de10935b59
Rado Andrianjanahary <rado.md92@gmail.com>
2022-04-08 22:12:03 +0200

    apply changes on definition linking, translate_english_definitions.py: use an english word list to prioritise some words over others

--- translate_english_definitions.py
+++ translate_english_definitions.py
@@ -1,3 +1,5 @@
+import os.path
+
 import pywikibot
 import requests
 
@@ -9,17 +11,26 @@ class DefinitionTranslationError(Exception):
     pass
 
 
+class BatchCreationError(DefinitionTranslationError):
+    pass
+
+
 class DefinitionTranslation(object):
     def __init__(self):
+        self.postgrest_url = '192.168.10.100:8080/api'
         self.botjagwar_frontend_url = '192.168.10.100:8080'
         self.translation_server = 'localhost:8003'
         self.definition_language = 'en'
         self.language = 'en'
 
         self._words_to_link = set()
+        self._english_wordlist = set()
+        self._current_file_size = 0
+        self._batch_file_counter = 1
+        self._current_file = None
 
     def get_words(self, language='en', page_number=1, words_per_page=10000):
-        url = f'http://{self.botjagwar_frontend_url}/api/unaggregated_dictionary'
+        url = f'http://{self.postgrest_url}/unaggregated_dictionary'
 
         pages = requests.get(url, params={
             'limit': words_per_page,
@@ -48,7 +59,7 @@ class DefinitionTranslation(object):
             return request.json()['text']
 
     def add_additional_data(self, word_id, type, information):
-        url = f'http://{self.botjagwar_frontend_url}/api/additional_word_information'
+        url = f'http://{self.postgrest_url}/additional_word_information'
         json = {
             'word_id': word_id,
             'type': type,
@@ -83,13 +94,16 @@ class DefinitionTranslation(object):
         """
         pass
 
-    def get_word_info_with_openmt_additional_data(self, items_per_page):
-        url = f'http://{self.botjagwar_frontend_url}/api/additional_word_information'
-        for page_number in range(100):
+    def get_word_info_with_openmt_additional_data(self, items_per_page, start_from_page=1):
+        total_pages = 100
+        url = f'http://{self.postgrest_url}/word_with_openmt_translation'
+        for page_number in range(total_pages):
+            print(f'(page {1+page_number}/{total_pages}) getting {items_per_page} pages from database...')
             pages = requests.get(url, params={
                 'limit': items_per_page,
-                'offset': items_per_page * page_number,
-                'type': f'eq.openmt_translation',
+                'offset': (start_from_page + page_number) * items_per_page,
+                'order': 'word_id',
+                'language': 'eq.en',
             })
             if pages.status_code != 200:
                 print(pages.json())
@@ -100,7 +114,7 @@ class DefinitionTranslation(object):
                 yield page
 
     def get_word_by_id(self, word_id):
-        url = f'http://{self.botjagwar_frontend_url}/api/unaggregated_dictionary'
+        url = f'http://{self.postgrest_url}/unaggregated_dictionary'
         pages = requests.get(url, params={
             'word_id': f'eq.{word_id}',
         })
@@ -111,7 +125,7 @@ class DefinitionTranslation(object):
         return pages.json()[0]
 
     def get_definitions(self, word_id):
-        url = f'http://{self.botjagwar_frontend_url}/api/unaggregated_dictionary'
+        url = f'http://{self.postgrest_url}/unaggregated_dictionary'
         pages = requests.get(url, params={
             'word_id': f'eq.{word_id}',
             'definition_language': f'eq.{self.definition_language}',
@@ -123,7 +137,7 @@ class DefinitionTranslation(object):
         return [line['definition'] for line in pages.json()]
 
     def mark_definition(self, word_id, mark_as='done'):
-        url = f'http://{self.botjagwar_frontend_url}/api/additional_word_information'
+        url = f'http://{self.postgrest_url}/additional_word_information'
         pages = requests.patch(url, params={
             'word_id': f'eq.{word_id}',
             'type': f'eq.openmt_translation',
@@ -135,7 +149,7 @@ class DefinitionTranslation(object):
             raise DefinitionTranslationError(pages.text)
 
     def get_translated_definitions(self, word_id):
-        url = f'http://{self.botjagwar_frontend_url}/api/additional_word_information'
+        url = f'http://{self.postgrest_url}/additional_word_information'
         pages = requests.get(url, params={
             'word_id': f'eq.{word_id}',
             'type': f'eq.openmt_translation',
@@ -147,56 +161,139 @@ class DefinitionTranslation(object):
         return [line['information'] for line in pages.json()]
 
     @property
+    def english_wordlist(self):
+        if self._english_wordlist:
+            return self._english_wordlist
+        else:
+            print('Loading 10,000 most common English words')
+            for word in open('user_data/list-en-1000.txt', 'r'):
+                word = word.strip()
+                self._english_wordlist.add(word.lower())
+
+            print('Loading complete!')
+            return self._english_wordlist
+
+    @property
     def malagasy_words_to_link(self):
         if self._words_to_link:
             return self._words_to_link
         else:
-            url = f'http://{self.botjagwar_frontend_url}/api/word'
+            print('Loading Malagasy words to link')
+            url = f'http://{self.postgrest_url}/word'
             pages = requests.get(url, params={
                 'language': f'eq.mg',
                 'part_of_speech': f'in.(ana,mat,mpam)',
                 'select': f'word',
             })
             if pages.status_code != 200:
-                raise DefinitionTranslationError(pages.text)
+                raise DefinitionTranslationError(pages.status_code)
             else:
                 self._words_to_link = {p['word'] for p in pages.json() if len(p['word']) > 4}
+                print(f'Loading complete! {len(self._words_to_link)} words loaded')
                 return self._words_to_link
 
+    def get_word_ids(self, words):
+        words_as_csl = ','.join(words)
+        url = f'http://{self.postgrest_url}/mv_word_with_openmt_translation'
+        pages = requests.get(url, params={
+            'lowercase_word': f'in.({words_as_csl})',
+            'language': 'eq.en',
+        })
+        if pages.status_code != 200:
+            print(pages.json())
+            raise DefinitionTranslationError(pages.text)
+
+        pages_as_json = pages.json()
+        for page in pages_as_json:
+            yield page
+
+    def run(self, word_additional_data_info, counter=0):
+        translation = Translation()
+        translation.output.wikipage_renderer.pages_to_link = self.malagasy_words_to_link
+        word = word_additional_data_info['word']
+        word_id = word_additional_data_info['word_id']
+        part_of_speech = word_additional_data_info['part_of_speech']
+        definitions = self.get_definitions(word_id)
+        translated_definitions = self.get_translated_definitions(word_id)
+        print(f'{word} ({word_id}) ->', definitions, translated_definitions)
+        entry = Entry(
+            entry=word,
+            part_of_speech=part_of_speech,
+            language=self.language,
+            definitions=translated_definitions
+        )
+        if not translated_definitions:
+            return
+
+        if translated_definitions and translated_definitions[0] == '.':
+            return
+
+        response = pywikibot.input(f'Entry # {counter + 1}: Accept and upload? (y/n)')
+        if response.strip() == 'y':
+            translation.publish_to_wiktionary(entry.entry, [entry])
+            translation._save_translation_from_page([entry])
+            self.mark_definition(word_id, 'done')
+        elif response.strip() == 'n':
+            self.mark_definition(word_id, 'rejected')
+        else:
+            return
 
     def publish_translated_definition(self):
         """
         Publish the translated definition on the target wiki.
         :return:
         """
-        definitions = []
-        translation = Translation()
-        translation.output.wikipage_renderer.pages_to_link = self.malagasy_words_to_link
+        counter = 0
+        words = []
+        for word_list_element in self.english_wordlist:
+            counter += 1
+            if not counter % 100:
+                print(counter)
 
-        for word_additional_data_info in self.get_word_info_with_openmt_additional_data(100):
-            word_info = self.get_word_by_id(word_additional_data_info['word_id'])
-            word_id = word_info['word_id']
-            word = word_info['word']
-            part_of_speech = word_info['part_of_speech']
-            definitions = self.get_definitions(word_id)
-            translated_definitions = self.get_translated_definitions(word_id)
-            print(f'{word} ({word_id}) ->', definitions, translated_definitions)
-            entry = Entry(
-                entry=word,
-                part_of_speech=part_of_speech,
-                language=self.language,
-                definitions=translated_definitions
-            )
-            response = pywikibot.input('Accept and upload? (y/n)')
-            if response.strip() == 'y':
-                translation.publish_to_wiktionary(entry.entry, [entry])
-                translation._save_translation_from_page([entry])
-                self.mark_definition(word_id, 'done')
-            elif response.strip() == 'n':
-                self.mark_definition(word_id, 'rejected')
-            else:
-                continue
-            # translation._save_translation_from_page(entries)
+            words.append(word_list_element)
+            if len(words) > 100:
+                for word_additional_data_info in self.get_word_ids(words):
+                    self.build_translation_batches(word_additional_data_info, counter)
+                    # self.run(word_additional_data_info, counter)
+                    # if not counter % 100:
+                    #     print('You can take a break now!')
+                words = []
+
+    def build_translation_batches(self, word_additional_data_info, counter=0):
+        translation = Translation()
+        batch_folder = 'user_data/translation_batch'
+        translation.output.wikipage_renderer.pages_to_link = self.malagasy_words_to_link
+        word = word_additional_data_info['word']
+        word_id = word_additional_data_info['word_id']
+        definitions = self.get_definitions(word_id)
+        translated_definitions = self.get_translated_definitions(word_id)
+        if not os.path.exists(batch_folder):
+            os.mkdir(batch_folder)
+
+        if os.path.exists(f'{batch_folder}/batch-{self._batch_file_counter}.csv'):
+            self._current_file = open(f'{batch_folder}/batch-{self._batch_file_counter}.csv', 'a')
+
+        if self._current_file is None:
+            if self._current_file_size >= 5000:
+                self._batch_file_counter += 1
+
+            filename = f'{batch_folder}/batch-{self._batch_file_counter}.csv'
+            self._current_file = open(filename, 'a')
+        else:
+            if self._current_file_size >= 5000:
+                self._current_file_size = 0
+                self._current_file.close()
+                self._batch_file_counter += 1
+                filename = f'{batch_folder}/batch-{self._batch_file_counter}.csv'
+                self._current_file = open(filename, 'a')
+
+        if len(definitions) > 0 and len(translated_definitions) > 0:
+            if translated_definitions and translated_definitions[0] == '.':
+                return
+
+            line = f'"{word}","{word_id}","{definitions[0]}","{translated_definitions[0]}"'
+            self._current_file.write(f'{line}\n')
+            self._current_file_size += len(definitions[0])
 
 
 if __name__ == '__main__':
