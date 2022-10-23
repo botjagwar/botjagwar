21c7f2078872ccb9d75c21ef7726cf744eaf4075
Rado Andrianjanahary <rado.md92@gmail.com>
2022-10-23 07:38:24 -0500

    test: comment out broken test

--- test/unit_tests/test_entryprocessor/test_fr.py
+++ test/unit_tests/test_entryprocessor/test_fr.py
@@ -1,6 +1,5 @@
 import unittest
 
-from test_utils.mocks import PageMock, SiteMock
 from . import GenericEntryProcessorTester
 
 
@@ -16,13 +15,13 @@ class TestFrenchWiktionaryEntryprocessor(
     def test_retrieve_translations(self):
         super(TestFrenchWiktionaryEntryprocessor, self).test_retrieve_translations()
 
-    def broken_test_retrieve_translations_data_output(self):
-        page = PageMock(SiteMock(self.language, 'wiktionary'), 'air')
-        self.processor.process(page)
-        entries = self.processor.retrieve_translations()
-        entry = [e for e in entries if e.language == 'ko'][0]
-        word, pos, lang, definition = entry.word, entry.part_of_speech, entry.language, entry.definition
-        self.assertEqual(word, '공기')
-        self.assertEqual(pos, 'ana')
-        self.assertEqual(lang, 'ko')
-        self.assertEqual(definition, 'air')
+    # def test_retrieve_translations_data_output(self):
+    #     page = PageMock(SiteMock(self.language, 'wiktionary'), 'air')
+    #     self.processor.process(page)
+    #     entries = self.processor.retrieve_translations()
+    #     entry = [e for e in entries if e.language == 'ko'][0]
+    #     word, pos, lang, definition = entry.word, entry.part_of_speech, entry.language, entry.definition
+    #     self.assertEqual(word, '공기')
+    #     self.assertEqual(pos, 'ana')
+    #     self.assertEqual(lang, 'ko')
+    #     self.assertEqual(definition, 'air')
