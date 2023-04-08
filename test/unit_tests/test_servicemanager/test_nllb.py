b7189f250abebcdbe726a8b88418c0e35a6176f6
Rado Andrianjanahary <rado.md92@gmail.com>
2023-04-08 21:27:15 +0200

    improve testability, add more tests

---
+++ test/unit_tests/test_servicemanager/test_nllb.py
@@ -0,0 +1,30 @@
+import unittest
+from unittest.mock import patch
+
+from api.servicemanager.nllb import NllbDefinitionTranslation, DefinitionTranslationError
+
+
+class TestNllbDefinitionTranslation(unittest.TestCase):
+
+    @patch('api.servicemanager.nllb.requests.get')
+    def test_get_translation_success(self, mock_get):
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = {'translated': 'Bonjour'}
+
+        nllb = NllbDefinitionTranslation('fr')
+        nllb.translation_server = 'localhost:12020'
+        result = nllb.get_translation('Hello')
+        self.assertEqual(result, 'Bonjour')
+
+        expected_url = 'http://localhost:12020/translate/en_Latn/fr_Latn'
+        expected_params = {'text': 'Hello'}
+        mock_get.assert_called_once_with(expected_url, params=expected_params)
+
+    @patch('api.servicemanager.nllb.requests.get')
+    def test_get_translation_failure(self, mock_get):
+        mock_get.return_value.status_code = 404
+        mock_get.return_value.text = 'Not Found'
+
+        nllb = NllbDefinitionTranslation('fr')
+        with self.assertRaises(DefinitionTranslationError):
+            nllb.get_translation('Hello')
